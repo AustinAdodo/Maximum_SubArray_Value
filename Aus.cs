327083f6c86dcd5ace16348c129ee35a405f58e3
Odia Adodo <subzbelow@gmail.com>
2023-05-03 23:37:07 +0100

    Final.

---
+++ Aus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum_SubArray_Value
+{
+    public class Aus
+    {
+        public static void Maximum_Subarray(int[] arr)
+        {
+            Dictionary<int[], int> result = new Dictionary<int[], int>();
+            //get aggregate of full array and gradually reduce aggregate map for each iteration.
+            //j is Zero based so 1 must be added for proper compensation.
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 1; j < arr.Length; j++)
+                {
+                    int[] temp = arr.Skip(i).Take(j + 1).ToArray();
+                    result[temp] = temp.Sum();
+                }
+            }
+            int[] resultkey = result.Where(a => a.Value == result.Values.Max()).FirstOrDefault().Key;
+            Console.Write("Subarray [{0}] the Maximum value is {1}", string.Join(",", resultkey), result.Values.Max().ToString());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Maximum_SubArray_Value
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] TestArr = { -2, -1, -4, 4, 7, 20, 1, 2, 5, -1, -4 };
+            Aus.Maximum_Subarray(TestArr);
+        }
+    }
+}
+

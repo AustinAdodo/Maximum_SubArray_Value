28061fd58a5aae158bebae3d608fffafddd1a061
Odia Adodo <subzbelow@gmail.com>
2023-05-04 06:01:53 +0100

    Benchmark Added

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Maximum_SubArray_Value;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+
+namespace Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var Test1Results = BenchmarkRunner.Run<SpeedTester>();
+        }
+
+        [MemoryDiagnoser(false)]
+        public class SpeedTester
+        {
+            [Params(10, 100, 1000)]
+            public int size { get; set; }
+
+            [Benchmark(Baseline = true)]
+            public void Test1()
+            {
+                int[] TestArr = { -2, -1, -4, 4, 7, 20, 1, 2, 5, -1, -4 };
+                Aus.Maximum_Subarray(TestArr);
+            }
+            [Benchmark] //open for scrutiny.
+            public void Test2()
+            {
+
+            }
+        }
+    }
+}

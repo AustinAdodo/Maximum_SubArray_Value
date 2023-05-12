bd1ae64c1fa491a79da2961e4471a216a217bcd1
Odia Adodo <subzbelow@gmail.com>
2023-05-12 15:58:43 +0100

    update 21-05-23

--- Program.cs
+++ Program.cs
@@ -7,9 +7,16 @@ namespace Maximum_SubArray_Value
         {
             //int[] TestArr = { -2, -1, -4, 4, 7, 20, 1, 2, 5, -1, -4 };
             //Aus.Maximum_Subarray(TestArr);
-            int[] arr = { 1, 4, 5, 2, 6 };
-            int[] arr1 = { 2,2,2,2,2 };
-            //Console.WriteLine(Aus1.findLIS(arr));   
+            //int[] arr = { 1, 4, 5, 2, 6 };
+            //int[] arr1 = { 2,2,2,2,2 };
+            string s = "ABCDE";
+            string result = String.Empty;
+            for (int i = s.Length; i-- > 0;)
+            {
+                result += s[i].ToString();
+            }
+            Console.WriteLine(result);
+            Console.Write(string.Join("",s.Reverse()));    
         }
     }
 }

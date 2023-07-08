f9363886cd352260c72901dcefda902c5cb6a384
Odia Adodo <subzbelow@gmail.com>
2023-07-08 14:39:50 +0100

    Test Attributes Defined.

--- Program.cs
+++ Program.cs
@@ -6,11 +6,6 @@ namespace Maximum_SubArray_Value
         static void Main(string[] args)
         {
             //int[] TestArr = { -2, -1, -4, 4, 7, 20, 1, 2, 5, -1, -4 };
-            //Aus.Maximum_Subarray(TestArr);
-
-            //Aus.FizzBuzz(15);
-            //Console.Write(Aus1.SpatialArrangement("a-bC-dEf=ghIj!!"));
-            //Console.Write(Aus1.SpatialArrangement("ab-cd"));
             Console.Write(Aus.Capitalize("optimus_prime"));
         }
     }

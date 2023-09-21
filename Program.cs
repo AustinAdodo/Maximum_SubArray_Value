e506a1880d67ad4bfb055c618b210a737bcee3ce
Odia Adodo <subzbelow@gmail.com>
2023-09-21 21:50:12 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -18,25 +18,21 @@ namespace Maximum_SubArray_Value
             string[] labels = {"legal","illegal", "legal", "illegal", "legal" };
             int dailyCount = 2;
             //Console.WriteLine(string.Join(",",Aus.FullJustify(arr,16)));
-            //List<Tuple<string, int>> tups = new List<Tuple<string, int>>();
-            //tups.Add(new Tuple<string, int>("Buck", 12));
-            //tups.Add(new Tuple<string, int>("Luck", 10));
-            //tups.Add(new Tuple<string, int>("Duck", 3));
-            //tups.Add(new Tuple<string, int>("Ruck", 6));
-            //tups.Add(new Tuple<string, int>("Durk", 16));
-            //tups.Add(new Tuple<string, int>("Shock", 12));
-            //var condition = tups.GroupBy(a => a).Any(group => group.Count() >= 2);
-            //var axe = tups.GroupBy(a => a).OrderBy(a => a.Key);
-            //if (condition) Console.Write(string.Join(",", tups.OrderBy(a => a.Item2).ThenBy(a => a.Item1).ToList()));
-            //else { Console.Write(string.Join(",", tups.OrderBy(a => a.Item2))); }
+            List<Tuple<string, int>> tups = new List<Tuple<string, int>>();
+            tups.Add(new Tuple<string, int>("Buck", 12));
+            tups.Add(new Tuple<string, int>("Luck", 10));
+            tups.Add(new Tuple<string, int>("Duck", 3));
+            tups.Add(new Tuple<string, int>("Ruck", 6));
+            tups.Add(new Tuple<string, int>("Durk", 16));
+            tups.Add(new Tuple<string, int>("Shock", 12));
+            var condition = tups.GroupBy(a => a).Any(group => group.Count() >= 2);
+            var axe = tups.GroupBy(a => a).OrderBy(a => a.Key);
+            if (condition) Console.Write(string.Join(",", tups.OrderBy(a => a.Item2).ThenBy(a => a.Item1).ToList()));
+            else { Console.Write(string.Join(",", tups.OrderBy(a => a.Item2))); }
             //bool result = concerned.Zip(concerned.Skip(1), (a, b) =>
             //Math.Abs(concerned.ToList().IndexOf(a) - concerned.ToList().IndexOf(b)) == 1).Any(a => a);
             ////Console.Write(result);
-            //Console.Write(String.Join(",",Aus.ChunkIter("FavouriteSkip",3)));
-            int[] arr1 = { 1, 1, 2, 2, 2 };
-            int[] arr2 = { 2,2,3,3,4};
-            int[] weight = { };
-            Console.Write(AusMain.maxCost(cost.ToList(),labels.ToList(),dailyCount)); 
+
             //Console.WriteLine(AusMain.MaxShared(4,arr1.ToList(),arr2.ToList(),weight.ToList()));
         }
     }
